7012d41e05d5fa7248e41a0880bc2ba0fc662bad
Maevus <maevus@gmail.com>
2019-02-25 20:02:51 +0000

    Initial Commit

---
+++ HelloPoly2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloPoly2
+//
+//  Created by Maeve Lynskey on 25/02/2019.
+//  Copyright © 2019 COMP47390. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
